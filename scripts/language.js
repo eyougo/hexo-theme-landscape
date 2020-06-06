93b550b134a445a91fccc5975a5348cc79a45553
eyougo <eyougo@gmail.com>
2020-06-06 12:06:27 +0800

    language bug

--- scripts/language.js
+++ scripts/language.js
@@ -1,12 +1,14 @@
 /**
  * 其他语言导航
  */
+var _ = require('lodash');
+
 hexo.extend.helper.register('url_for_lang', function(lang){
     var p = this.page.path;
     var root = this.config.root || '';
     var language = this.config.language;
     if (_.startsWith(p, language)) {
-        p = p.substring(language);
+        p = p.substring(language.length);
     }
     if (!_.startsWith(p, '/')){
         p = '/' + p;
