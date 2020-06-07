3a2204172cdbf7ff4c8303ab87927945392b56a6
eyougo <eyougo@gmail.com>
2020-06-07 23:33:43 +0800

    language bug

--- scripts/language.js
+++ scripts/language.js
@@ -5,13 +5,14 @@ var _ = require('lodash');
 
 hexo.extend.helper.register('url_for_lang', function(lang){
     var p = this.page.path;
-    var root = this.config.root || '';
     var language = this.config.language;
+    console.log("url:", this.url_for(p));
     if (_.startsWith(p, language)) {
         p = p.substring(language.length);
     }
+    console.log("page path substring:", p);
     if (!_.startsWith(p, '/')){
         p = '/' + p;
     }
-    return root + lang + p;
+    return '/' + lang + p;
 });
